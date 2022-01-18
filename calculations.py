de8de00f28a4634b279dffce0758b5489a50e29b
Kristina Donaldson <donaldsonkristinar@yahoo.com>
2022-01-17 20:43:39 -0700

    first commit

---
+++ calculations.py
@@ -0,0 +1,6 @@
+def house_area(length_of_house, width_of_house):
+    print(length_of_house * width_of_house)
+    
+def circle_circumference(radius_of_circle):
+    print(3.14 * (radius_of_circle**2))
+    
